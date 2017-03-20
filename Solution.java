65367018c69e6eb2163c64ea5b685bdd373e6474
Zoeyyyyyy <zoeyzhoujob@gmail.com>
2017-03-20 07:02:41 -0400

    Update Comment

--- Solution.java
+++ Solution.java
@@ -18,27 +18,28 @@ public class Solution {
 	 * Electronic  2%
 	 * Else        0
 	 */	
-	private static double Flatfee = 1.05;
-	private static double Labourfee = 0.012;
-	private static double Drugfee = 0.075;
-	private static double Foodfee = 0.13;
-	private static double Electronicsfee = 0.02;
+    // Initial markup %
+    private static double Flatfee = 1.05;
+    private static double Labourfee = 0.012;
+    private static double Drugfee = 0.075;
+    private static double Foodfee = 0.13;
+    private static double Electronicsfee = 0.02;
 	
-	public static double Flatrate;
-	public static double price;
-	public static double numofpeople;
-	public static String ProductType;
- 
+    public static double Flatrate;
+    public static double price;
+    public static double numofpeople;
+    public static String ProductType;
+    // get flat markup
     public static double getFlatrate(double price) {
     	Flatrate = price * Flatfee;
     	return Flatrate;
 	}
-    
+    // get  # of people
     public static int getNumberofpeople(double numofpeople) {
     	return (int) numofpeople;
     }
-   
-   public static double getProdcutType(String ProductType) {
+    // Calculate final payment according to category
+    public static double getProdcutType(String ProductType) {
 	   if (ProductType != "drugs" && ProductType != "food" && ProductType != "electronics") {
 		   ProductType = "else";
 	   }
@@ -59,16 +60,17 @@ public class Solution {
        }     
 	return payment;
    }
-    
+	
+    // Change Test Case Here
     public static void setParameters() {
-    	price = 12456.95;
-    	numofpeople = 4;
-    	ProductType = "books";
+    	price = 12456.95;   // set price of product
+    	numofpeople = 4;    // set # of People
+    	ProductType = "books";  //Set category
     }
     
 	public static void main(String[] args) {
-		setParameters();
-		NumberFormat output = new DecimalFormat("$###,###,###.##");
+		setParameters();  //read product information		
+		NumberFormat output = new DecimalFormat("$###,###,###.##");  // set Format
 		String Price = output.format(price);
 		String Payment = output.format(getProdcutType(ProductType));
 		System.out.println("Input: " + Price +", " + getNumberofpeople(numofpeople) + " people, " + ProductType);
